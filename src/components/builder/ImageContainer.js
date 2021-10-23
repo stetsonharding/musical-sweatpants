cdbb83ac79c1d5e2c220f15de82c5ad0ece8d249
Stetson Harding <stetson2001@msn.com>
2021-10-22 20:59:37 -0700

    added stylesheet to image component, fix babel.

--- src/components/builder/ImageContainer.js
+++ src/components/builder/ImageContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// import "./css/ImageContainer.css";
+import "../../css/ImageContainer.js";
 
 //sweatpant images
 import frontSide from "../../assets/frontside.png";
@@ -8,29 +8,10 @@ import backSide from "../../assets/backside.png";
 
 function ImageContainer() {
   return (
-    <div
-      className="sweatpants-container"
-      style={{ backgroundColor: "lightgrey", width: "50%", height: "100%" }}
-    >
-      <div
-        className="sweapants-images"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-        }}
-      >
-        <img
-          style={{ margin: "40px" }}
-          src={frontSide}
-          alt="Front View of Sweatpants"
-        />
-        <img
-          style={{ margin: "40px" }}
-          src={backSide}
-          alt="Back View of Sweatpants"
-        />
+    <div className="sweatpants-container">
+      <div className="sweapants-images">
+        <img src={frontSide} alt="Front View of Sweatpants" />
+        <img src={backSide} alt="Back View of Sweatpants" />
       </div>
     </div>
   );
