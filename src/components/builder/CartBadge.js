67f59f48a5c25b059123f9fd94f32cadb1d0b54b
Stetson Harding <stetson2001@msn.com>
2021-11-06 09:03:19 -0700

    implemented quick cart plus styles.

--- src/components/builder/CartBadge.js
+++ src/components/builder/CartBadge.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../css/CartBadge.css";
 
 function CartBadge() {
-  return <div className="cart-badge"></div>;
+  const [isQuickCartShown, setIsQuickCartShown] = useState(false);
+
+  const showQuickCart = () => {
+    setIsQuickCartShown(!isQuickCartShown);
+  };
+
+  return (
+    <>
+      <div
+        onClick={showQuickCart}
+        className="cart-badge"
+        style={{
+          border: isQuickCartShown ? "1px solid rgb(7, 188, 233)" : null,
+        }}
+      ></div>
+      ;{isQuickCartShown && <QuickCartView />}
+    </>
+  );
+}
+
+function QuickCartView() {
+  return <div className="quick-cart-container"></div>;
 }
 
 export default CartBadge;
