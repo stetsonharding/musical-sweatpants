0a0bce6e198301e2676e046a6d29a9039f434d63
stetsonharding <stetson2001@msn.com>
2021-11-27 09:06:27 -0800

    Extacted Album Cover and Name from selected album.

--- src/components/builder/ImageContainer.js
+++ src/components/builder/ImageContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "../../css/ImageContainer.css";
 
 //sweatpant images
 import frontSide from "../../assets/frontside.png";
 import backSide from "../../assets/backside.png";
+import { useCurrentAlbumContext } from "../../providers/CurrentAlbumProvider";
 
 function ImageContainer() {
+  const [currentAlbum] = useCurrentAlbumContext();
+
+  console.log(currentAlbum);
+
   return (
     <div className="sweatpants-container">
       <div className="sweapants-images">
+        <img
+          src={currentAlbum && currentAlbum.artworkUrl60}
+          alt={currentAlbum && "Album Cover"}
+        />
+        <h4> {currentAlbum && currentAlbum.artistName}</h4>
         <img
           className="sweatpants_frontview"
           src={frontSide}
