3428335fe54db79ad0c6a5455663d4e77f026297
stetsonharding <stetson2001@msn.com>
2022-06-17 06:01:20 -0700

    removed payment and shipping forms.

--- src/components/orderInfo/OrderInfoLayout.js
+++ src/components/orderInfo/OrderInfoLayout.js
@@ -1,26 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 //components
 import CartBadge from "../builder/CartBadge";
 import OrderDetails from "./OrderDetails";
-import ShippingForm from "./ShippingForm"
-import BillingForm from "./BillingForm";
-import PaymentForm from "./PaymentForm";
+import ShippingForm from "./ShippingForm";
+
 //css
-import "../../css/OrderInfoLayout.css"
+import "../../css/OrderInfoLayout.css";
 
 export default function OrderInfoLayout() {
-  //State to condionally render forms.
-  const [isFormShown, setIsFormShown] = useState(0)
-
   return (
-    <div className='order-info-container'>
+    <div className="order-info-container">
       <div>
-      <CartBadge />
+        <CartBadge />
       </div>
       <div className="order-info">
-        <OrderDetails /> 
-        {/* Conditionally rendering checkout forms */}
-        {isFormShown === 0 ? <ShippingForm setIsFormShown={setIsFormShown}/> : isFormShown === 1 ? <BillingForm setIsFormShown={setIsFormShown}/> : isFormShown === 2 ? <PaymentForm /> : null }
+        <OrderDetails />
+
+        <ShippingForm />
       </div>
     </div>
   );
--- src/components/orderInfo/ShippingForm.js
+++ src/components/orderInfo/ShippingForm.js
@@ -1,51 +1,62 @@
-import React from 'react';
+import React from "react";
 //Context
 import { useOrderFormContext } from "../../providers/OrderFormProvider";
 //Components
-import FormInput from "./FormInput"
+import FormInput from "./FormInput";
 //css
-import "../../css/FormInput.css"
+import "../../css/FormInput.css";
 
-function ShippingForm({setIsFormShown}) {
-    const {shippingData, setShippingData} = useOrderFormContext();
-    return (
-        <>
-        <div className="form-container">
-            <h2 className="title-bold" style={{textAlign: 'center'}}>Shipping Information</h2>
-            <FormInput
-            formData={shippingData}
-            inputData={['firstname', 'lastname']}
-            setInputData={setShippingData}
-            placeholder={['First Name', 'Last Name']}
-            />
-            <FormInput
-            formData={shippingData}
-            inputData={['address']}
-            setInputData={setShippingData}
-            placeholder={['Address']}
-            />
-            <FormInput
-            formData={shippingData}
-            inputData={['subaddress']}
-            setInputData={setShippingData}
-            placeholder={['Sub Address']}
-            />
-            <FormInput
-            formData={shippingData}
-            inputData={['city']}
-            setInputData={setShippingData}
-            placeholder={['City']}
-            />
-            <FormInput
-            formData={shippingData}
-            inputData={['county', 'state', 'zip']}
-            setInputData={setShippingData}
-            placeholder={['Country', 'State', 'Zip']}
-            />
-            {/* Conditionally render billing form upon click*/}
-            <button className="action-btn" onClick={() => setIsFormShown(1)}>Continue to Billing</button>
-        </div>
-        </>
-    )
+import { useNavigate } from "react-router-dom";
+
+function ShippingForm({ setIsFormShown }) {
+  const { shippingData, setShippingData } = useOrderFormContext();
+
+  const history = useNavigate();
+  return (
+    <>
+      <div className="form-container">
+        <h2 className="title-bold" style={{ textAlign: "center" }}>
+          Shipping Information
+        </h2>
+        <FormInput
+          formData={shippingData}
+          inputData={["firstname", "lastname"]}
+          setInputData={setShippingData}
+          placeholder={["First Name", "Last Name"]}
+        />
+        <FormInput
+          formData={shippingData}
+          inputData={["address"]}
+          setInputData={setShippingData}
+          placeholder={["Address"]}
+        />
+        <FormInput
+          formData={shippingData}
+          inputData={["subaddress"]}
+          setInputData={setShippingData}
+          placeholder={["Sub Address"]}
+        />
+        <FormInput
+          formData={shippingData}
+          inputData={["city"]}
+          setInputData={setShippingData}
+          placeholder={["City"]}
+        />
+        <FormInput
+          formData={shippingData}
+          inputData={["county", "state", "zip"]}
+          setInputData={setShippingData}
+          placeholder={["Country", "State", "Zip"]}
+        />
+        {/* Conditionally render billing form upon click*/}
+        <button
+          className="action-btn"
+          onClick={() => history("/orderComplete")}
+        >
+          Place Order
+        </button>
+      </div>
+    </>
+  );
 }
-export default ShippingForm
+export default ShippingForm;
